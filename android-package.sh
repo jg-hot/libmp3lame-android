8e8bf32d3c9786c4f298255e1ab193d7b7c16451
John Hotovy <jghotovy92@gmail.com>
2024-11-21 18:27:45 -0700

    Refactor packaging script

--- android-package.sh
+++ android-package.sh
@@ -3,21 +3,21 @@ ABIS=("armeabi-v7a" "arm64-v8a" "x86" "x86_64")
 
 # ./build_all_android.sh
 
-rm -r build/prefab
-mkdir -p build/prefab
+rm -r build/aar/prefab/
+mkdir -vp build/aar/prefab/
+
+# copy metadata files
+cp -vrt build/aar/prefab/ android/prefab-template/*
 
 # make prefab package
 pushd build
-    # copy metadata files
-    cp -vr ../android/prefab-template/* prefab/
-
     # copy headers
-    mkdir -vp prefab/modules/mp3lame/include
-    cp -vt prefab/modules/mp3lame/include include/lame/*.h
+    mkdir -vp aar/prefab/modules/mp3lame/include/
+    cp -vt aar/prefab/modules/mp3lame/include/ include/lame/*.h
 
     # copy libraries
     for ABI in ${ABIS[@]}; do
-        cp -vt prefab/modules/mp3lame/libs/android.$ABI $ABI/lib/*.so
+        cp -vt aar/prefab/modules/mp3lame/libs/android.$ABI/ $ABI/lib/*.so
     done
 
     # verify prefab package
@@ -29,8 +29,8 @@ pushd build
             --ndk-version 27 \
             --stl none \
             --abi ${ABI} \
-            --output $(pwd)/prefab-verification \
-            $(pwd)/prefab)
+            --output $(pwd)/tmp/prefab-verification \
+            $(pwd)/aar/prefab)
 
         RESULT=$?; if [[ $RESULT == 0 ]]; then
             echo "$ABI: prefab package verified"
@@ -39,16 +39,12 @@ pushd build
             exit 1
         fi
 
-        rm -r prefab-verification/
+        rm -r tmp/prefab-verification/
     done
 popd
 
 # zip prefab/ and AndroidManifest.xml into an .aar
-rm -r build/aar
-mkdir -p build/aar
-
-cp -rt build/aar/ build/prefab
-cp -t build/aar/ android/AndroidManifest.xml
+cp -vt build/aar/ android/AndroidManifest.xml
 
 pushd build/aar
     zip -r ../lame-3.100.aar . > /dev/null
